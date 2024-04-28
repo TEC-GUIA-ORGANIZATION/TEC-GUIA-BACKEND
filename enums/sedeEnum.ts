4d928b4ba5c6455f96a8b90e6c7afb3a340bdbd5
ERodbot <emarodriguez@estudainec.cr>
2024-04-28 17:02:38 -0600

    schemas inciales para usuarios

---
+++ enums/sedeEnum.ts
@@ -0,0 +1,9 @@
+enum Sede {
+    Cartago = 'Cartago',
+    Sanjose = 'San Jose',
+    Sancarlos = 'San Carlos',
+    Alajuela = 'Alajuela',
+    Limon = 'Limon',
+}
+
+module.exports = Sede;
---
+++ models/profesorGuiaModel.js
@@ -0,0 +1,35 @@
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//inheritance from usuarioModel 
+const Usuario = require('./usuarioModel');
+
+const profesorGuiaSchema = new Schema({
+    codigo: {
+        type: String,
+        required: true
+    },
+    telegonoOficina: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    telefonoPersonal: {
+        type: String,
+        required: false,
+        unique: true
+    },
+    esCoordinador: {
+        type: Boolean,
+        required: true
+    },
+    estaActivo: {
+        type: Boolean,
+        required: true
+    },
+})
+
+
+const ProfesorGuia = mongoose.model('ProfesorGuia', userSchema);
+module.exports = ProfesorGuia
