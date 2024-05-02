c00ec7ac78e6e47057221286434eb605efaaecb6
ERodbot <emarodriguez@estudiantec.cr>
2024-05-01 23:35:32 -0600

    minor change 4

--- libs/jwt.js
+++ libs/jwt.js
@@ -5,6 +5,7 @@ function createAccessToken(payload) {
     return new Promise((resolve, reject) => {
         jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
             if (err) reject(err);
+            console.log("fireit!");
             resolve(token);
         });
     });
