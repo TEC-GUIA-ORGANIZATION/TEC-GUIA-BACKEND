f2c96c8375d3e38cd75b3bacb4192695a5271667
ERodbot <emarodriguez@estudiantec.cr>
2024-05-02 10:27:20 -0600

    minor change on jwt

--- libs/jwt.js
+++ libs/jwt.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 function createAccessToken(payload) {
     return new Promise((resolve, reject) => {
         jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
             if (err) reject(err);
-            console.log("fireit!");
             resolve(token);
         });
     });
