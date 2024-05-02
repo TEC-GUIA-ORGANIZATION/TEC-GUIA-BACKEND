1037dce28de6ec3502d82391ab1972ba7cf0915a
ERodbot <emarodriguez@estudiantec.cr>
2024-05-02 10:27:20 -0600

    minor change on jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,15 +51,13 @@ const register = async (req, res) => {
         const token = await createAccessToken({
             id: usuarioSaved._id,
         });
-
-        res.json({errorJson: 'got res before expected'})
-
+        
         res.cookie("token", token);
     
-        //res.json({
-            //id: usuarioSaved._id,
-            //email: usuarioSaved.correo,
-        //});
+        res.json({
+            id: usuarioSaved._id,
+            email: usuarioSaved.correo,
+        });
 
     } catch (error) {
         res.status(500).json({ message: error.message});
