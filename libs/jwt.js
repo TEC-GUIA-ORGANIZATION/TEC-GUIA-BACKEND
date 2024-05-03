5f8520b47849469b1f07c39528014ef298a79582
ERodbot <emarodriguez@estudiantec.cr>
2024-05-02 19:44:35 -0600

    test for dotenv variable not being accessed v2

--- libs/jwt.js
+++ libs/jwt.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const jwt = require("jsonwebtoken");
-
+const TOKEN_SECRET =  "secret";
 
 function createAccessToken(payload) {
     return new Promise((resolve, reject) => {
-        jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
+        jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
             if (err) reject(err);
             resolve(token);
         });
