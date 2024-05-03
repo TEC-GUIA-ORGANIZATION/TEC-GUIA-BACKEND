859deb08dfe30f6e4f0fa341809c04db565c527c
Johanx22x <johanrodsa2210@gmail.com>
2024-05-02 19:15:44 -0600

    Remove some declarations from CORS

--- server.js
+++ server.js
@@ -26,9 +26,6 @@ app.use(cors());
 
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
 }));
 
 app.use((req, res, next) => {
