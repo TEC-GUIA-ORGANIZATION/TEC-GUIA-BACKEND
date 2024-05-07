67822dc21e2cc4c35688939a0872ad08a2f6e3de
Rafael Vargas <vbr.vargas31@gmail.com>
2024-05-06 21:05:48 -0600

    Cambios auth.controller.ts tipos de usuario

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,14 +1,9 @@
 import { IUser, UsuarioModel as User } from '../presentation/Models/usuario.model';
-import bcrypt from "bcrypt";
-import { createAccessToken } from '../libs/jwt';
 import jwt from 'jsonwebtoken';
 import { ProfesorGuiaModel } from '../presentation/Models/profesorGuia.model';
 import { AsistenteAdministradorModel } from '../presentation/Models/asistenteAdministrador.model';
-import { campus } from '../presentation/Models/usuario.model';
-import { config } from 'dotenv';
 import { Request, Response } from 'express';
 import { envs } from "../config/envs";
-import { CLIENT_RENEG_WINDOW } from 'tls';
 
 export class AuthController {
 
@@ -20,12 +15,21 @@ export class AuthController {
         try {
             const newUser: IUser = new User(req.body);
             newUser.password = await newUser.encryptPassword(newUser.password);
+
+            // if (newUser.rol === 'admin') {
+            //     const idAdmin = await this.createAdmin(req, res);
+            //     newUser.adminInfo = idAdmin.toString();
+            // }
+                
+
+
+
             const savedUser = await newUser.save();
             //*Token
             const token: string = jwt.sign({ _id: savedUser._id }, envs.TOKEN_SECRET || 'tokentest', {
                 expiresIn: 60 * 60 * 24
             });
-            res.header('auth-token', token).json(savedUser);
+            res.header('auth-token', token).json(savedUser);  
         } catch (error) {
             res.status(500).json({ error: 'Error al obtener los datos' });
         }
@@ -52,6 +56,34 @@ export class AuthController {
         if(!user) 
             return res.status(404).json('Usuario no encontrado');
         res.json(user);
+    }
+
+    public createAdmin = async(req: Request, res: Response): Promise<string> => {
+        try {
+            const { esPrincipal } = req.body;
+            const newAdminInfo = new AsistenteAdministradorModel({ esPrincipal });
+            const adminInfoSaved = await newAdminInfo.save();
+            return adminInfoSaved._id.toString();
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    public createProfesorGuia = async (req: Request, res: Response): Promise<string> => {
+        try {
+            const { codigo, telefonoOficina, telefonoPersonal, esCoordinador, estaActivo } = req.body;
+            const newProfesorGuiaInfo = new ProfesorGuiaModel({
+                codigo,
+                telefonoOficina,
+                telefonoPersonal,
+                esCoordinador,
+                estaActivo
+            });
+            const profesorGuiaSaved = await newProfesorGuiaInfo.save();
+            return profesorGuiaSaved._id.toString();
+        } catch (error) {
+            throw error;
+        }
     }
 
 }
