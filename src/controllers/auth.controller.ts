d5b9228980ab4751cfb3372ff88bc25732afdd16
ERodbot <emarodriguez@estudiantec.cr>
2024-05-07 20:04:32 -0600

    update on users: implemented inheritance but profesor guia functionality still on development

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,10 @@
 import { IUser, UsuarioModel as User } from '../presentation/Models/usuario.model';
 import jwt from 'jsonwebtoken';
-import { ProfesorGuiaModel } from '../presentation/Models/profesorGuia.model';
-import { AsistenteAdministradorModel } from '../presentation/Models/asistenteAdministrador.model';
-import { Request, Response } from 'express';
+import { IGuideProfessor, ProfesorGuiaModel as GuideProfesor} from '../presentation/Models/usuario.model';
+import { AsistenteAdministradorModel as Admin} from '../presentation/Models/usuario.model';
+import { Request, Response } from 'express'
 import { envs } from "../config/envs";
-
+import { rol } from "../presentation/Models/usuario.model";
 export class AuthController {
 
     public signUp = async (req: Request, res: Response) => {
@@ -13,23 +13,28 @@ export class AuthController {
         if (emailExist) return res.status(400).json('Correo ya existe');
 
         try {
-            const newUser: IUser = new User(req.body);
-            newUser.password = await newUser.encryptPassword(newUser.password);
-
-            // if (newUser.rol === 'admin') {
-            //     const idAdmin = await this.createAdmin(req, res);
-            //     newUser.adminInfo = idAdmin.toString();
-            // }
-                
 
 
-
-            const savedUser = await newUser.save();
-            //*Token
-            const token: string = jwt.sign({ _id: savedUser._id }, envs.TOKEN_SECRET || 'tokentest', {
-                expiresIn: 60 * 60 * 24
+            if ({ rol: req.body.rol === rol.ADMIN}) {
+                const newUser:IUser  = new Admin(req.body);
+                newUser.password = await newUser.encryptPassword(newUser.password);
+                const savedUser = await newUser.save();
+                //Token
+                const token: string = jwt.sign({ _id: savedUser._id }, envs.TOKEN_SECRET || 'tokentest', {
+                    expiresIn: 60 * 60 * 24
             });
-            res.header('auth-token', token).json(savedUser);  
+                res.header('auth-token', token).json(savedUser);  
+            } else if ({ rol: req.body.rol ===  rol.PROFESOR_GUIA}) {
+                console.log('here');
+                const newUser:IGuideProfessor  = new GuideProfesor(req.body);
+                newUser.password = await newUser.encryptPassword(newUser.password);
+                const savedUser = await newUser.save();
+                //Token
+                const token: string = jwt.sign({ _id: savedUser._id }, envs.TOKEN_SECRET || 'tokentest', {
+                    expiresIn: 60 * 60 * 24
+                });
+                res.header('auth-token', token).json(savedUser); 
+            }
         } catch (error) {
             res.status(500).json({ error: 'Error al obtener los datos' });
         }
@@ -58,32 +63,5 @@ export class AuthController {
         res.json(user);
     }
 
-    public createAdmin = async(req: Request, res: Response): Promise<string> => {
-        try {
-            const { esPrincipal } = req.body;
-            const newAdminInfo = new AsistenteAdministradorModel({ esPrincipal });
-            const adminInfoSaved = await newAdminInfo.save();
-            return adminInfoSaved._id.toString();
-        } catch (error) {
-            throw error;
-        }
-    }
-    
-    public createProfesorGuia = async (req: Request, res: Response): Promise<string> => {
-        try {
-            const { codigo, telefonoOficina, telefonoPersonal, esCoordinador, estaActivo } = req.body;
-            const newProfesorGuiaInfo = new ProfesorGuiaModel({
-                codigo,
-                telefonoOficina,
-                telefonoPersonal,
-                esCoordinador,
-                estaActivo
-            });
-            const profesorGuiaSaved = await newProfesorGuiaInfo.save();
-            return profesorGuiaSaved._id.toString();
-        } catch (error) {
-            throw error;
-        }
-    }
 
 }
