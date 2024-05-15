775f139c5c340874a5b3f7ce61989d4a1159d37d
Rafael Vargas <vbr.vargas31@gmail.com>
2024-05-15 17:05:13 -0600

    cambios en student.routes

--- src/presentation/Routes/studentsList.routes.ts
+++ src/presentation/Routes/studentsList.routes.ts
@@ -12,7 +12,6 @@ export class StudentsListRoutes {
         const studentsController = new StudentsController();
         const upload = multer({dest: 'uploads/'});
 
-<<<<<<< HEAD
         router.post('/uploadStudentLists', studentsController.uploadStudentList); //* Crear
         router.patch('/updateStudent/:id', studentsController.updateStudent); //* Crear
         router.get('/getAllStudentsInPeriod', studentsController.getAllStudents); //* Crear
@@ -20,15 +19,6 @@ export class StudentsListRoutes {
         // router.delete('/:id', activitiesController.deleteActivity); //* Eliminar
         // router.patch('/:id', activitiesController.updateActivity) //* Actualizar 
         router.post('/upload', upload.single('file'), studentsController.saveStudentsFromExcel);
-=======
-        router.post('/uploadStudentLists', studentsController.uploadStudentList); //* Subir lista cargada de estudiantes
-        router.patch('/updateStudent/:id', studentsController.updateStudent); //* Actualizar info de estudiante
-        router.get('/getAllStudentsInPeriod', studentsController.getAllStudents); //* Obtener todos los estudiantes
-        router.get('/getAllStudentsInPeriodByCampus', studentsController.getAllStudentsByCampus); //* Obtener todos los estudiantes por campo
-        router.get('/getStudentsOrderedByAlphabeticOrder', studentsController.getAllStudentsOrderByName); //* Obtener todos los estudiantes por campo
-        router.get('/getStudentsOrderedByCampus', studentsController.getAllStudentsOrderByCampus); //* Obtener todos los estudiantes por campo
-        router.get('/getStudentsOrderedByInstitutionId', studentsController.getAllStudentsOrderByInstitutionId); //* Obtener todos los estudiantes por campo
->>>>>>> 874a3568c46af1348bcc5ce0b1be6d48306663a2
         return router;
     }
 }
