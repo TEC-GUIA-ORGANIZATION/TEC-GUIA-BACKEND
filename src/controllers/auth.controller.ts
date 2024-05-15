54f14d7af64b66762a35a420975a91c7cc2b89b8
Johanx22x <johanrodsa2210@gmail.com>
2024-05-15 12:35:52 -0600

    fix conflicts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,22 +5,19 @@ import { ProfesorGuiaModel as ProfesorUser } from '../presentation/Models/profes
 import { Request, Response } from 'express';
 import { envs } from "../config/envs";
 import { rol } from "../utils/rol.enum";
+
 export class AuthController {
-
-
-
     public signUp = async (req: Request, res: Response) => {
-
         const emailExist = await User.findOne({ email: req.body.email });
         if (emailExist) return res.status(400).json('Correo ya existe');
 
         try {
-           
             const savedUser = await this.assingRol(req, res);
             await savedUser.save();
-            //*Token
+
             const token: string = jwt.sign({ _id: savedUser._id }, envs.TOKEN_SECRET || 'tokentest', {
                 expiresIn: 60 * 60 * 24});
+
             res.header('auth-token', token).json(savedUser); 
         } catch (error) {
             res.status(500).json(error);
@@ -28,7 +25,6 @@ export class AuthController {
     }
 
     public signIn = async (req: Request, res: Response) => {
-
         const user = await User.findOne({ email: req.body.email });
         if (!user) return res.status(400).json('El email no es válido.');
 
@@ -49,6 +45,7 @@ export class AuthController {
         const user = await User.findById(req.userId);
         if(!user) 
             return res.status(404).json('Usuario no encontrado');
+
         res.json(user);
     }
 
@@ -61,5 +58,4 @@ export class AuthController {
         newUser.password = await newUser.encryptPassword(newUser.password);
         return newUser;
     } 
-
 }
