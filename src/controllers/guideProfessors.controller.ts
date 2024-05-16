5aadeae2e05ab3a41f8d48b99178060050649430
ERodbot <emarodriguez@estudiantec.cr>
2024-05-16 10:22:16 -0600

    not that necessary commit, only for cleaning status

--- src/controllers/guideProfessors.controller.ts
+++ src/controllers/guideProfessors.controller.ts
@@ -1,57 +1,50 @@
 import { Request, Response } from "express";
 import mongoose from 'mongoose';
 import { ProfesorGuiaModel } from '../presentation/Models/profesorGuia.model';
-
+import { rol } from '../utils/rol.enum';
 
 export class GuideProfesorsController {
 
 
-public changeActiveStatusProfessor = async (req: Request, res: Response) => {
-    const { rol, name, firstLastname, secondLastname, email, officePhone, personalPhone, code } = req.body;
-    // Validate required fields
-    if (!rol || !name || !firstLastname || !secondLastname || !email || !officePhone || !personalPhone || !code) {
-        return res.status(400).json({ error: "Todos los campos son requeridos" });
-    }
-
-    try {
-        // Find the professor
-        const professorToBeActivated = await ProfesorGuiaModel.findOne({
-            email,
-            name,
-            firstLastname,
-            secondLastname,
-            officePhone,
-            personalPhone,
-            rol,
-            code,
-        });
-
-        if (!professorToBeActivated) {
-            res.status(404).json({ error: "Professor not found." });
-        } else {
-            // Toggle the isActive status
-            const updatedProfessor = await ProfesorGuiaModel.findByIdAndUpdate(
-                professorToBeActivated._id,
-                { isActive: !professorToBeActivated.isActive },
-                { new: true }
-            );
-            if (!updatedProfessor) {
-                res.status(500).json({ error: "Fallo la actualizacion del profesor" });
-            }
-            // Respond with the updated professor
-            res.status(200).json({ message: "Estado de profesor actualizado correctamente", updatedProfessor });
+    public changeActiveStatusProfessor = async (req: Request, res: Response) => {
+        const { rol, name, firstLastname, secondLastname, email, officePhone, personalPhone, code } = req.body;
+        // Validate required fields
+        if (!rol || !name || !firstLastname || !secondLastname || !email || !officePhone || !personalPhone || !code) {
+            return res.status(400).json({ error: "Todos los campos son requeridos" });
         }
-    } catch (error) {
-        console.error("Error cambiando estatus ", error);
-        res.status(500).json({ error: "An error occurred while changing the active status of the professor." });
+
+        try {
+            // Find the professor
+            const professorToBeActivated = await ProfesorGuiaModel.findOne({
+                email,
+                name,
+                firstLastname,
+                secondLastname,
+                officePhone,
+                personalPhone,
+                rol,
+                code,
+            });
+
+            if (!professorToBeActivated) {
+                res.status(404).json({ error: "Professor not found." });
+            } else {
+                // Toggle the isActive status
+                const updatedProfessor = await ProfesorGuiaModel.findByIdAndUpdate(
+                    professorToBeActivated._id,
+                    { isActive: !professorToBeActivated.isActive },
+                    { new: true }
+                );
+                if (!updatedProfessor) {
+                    res.status(500).json({ error: "Fallo la actualizacion del profesor" });
+                }
+                // Respond with the updated professor
+                res.status(200).json({ message: "Estado de profesor actualizado correctamente", updatedProfessor });
+            }
+        } catch (error) {
+            console.error("Error cambiando estatus ", error);
+            res.status(500).json({ error: "An error occurred while changing the active status of the professor." });
+        }
     }
-}
-
-  
-      
-        
-
-
     
-
 }
