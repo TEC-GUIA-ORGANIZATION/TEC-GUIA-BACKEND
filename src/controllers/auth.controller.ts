fcbeafd0542fb23b03d274d3a088682b2b039db7
kenfabguzram <kendallguzmanramirez@gmail.com>
2024-06-14 10:30:28 -0600

    added auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,6 @@
-import { IUser, UsuarioModel as User } from '../presentation/Models/usuario.model';
+import { IUser, UsuarioModel as User, UsuarioModel } from '../presentation/Models/usuario.model';
+import { IAuthenticable } from '../presentation/Models/authenticable.interface';
+import { IAuthenticableWrapper,AuthenticableWrapperModel as AuthenticableWrapper } from '../presentation/Models/authenticableWrapper.model';
 import jwt from 'jsonwebtoken';
 import { AdminAssistantModel as AdminUser } from '../presentation/Models/asistenteAdministrador.model';
 import { ProfesorGuiaModel as ProfesorUser } from '../presentation/Models/profesorGuia.model';
@@ -6,56 +8,52 @@ import { Request, Response } from 'express';
 import { envs } from "../config/envs";
 import { rol } from "../utils/rol.enum";
 
-export class AuthController {
-    public signUp = async (req: Request, res: Response) => {
-        const emailExist = await User.findOne({ email: req.body.email });
-        if (emailExist) return res.status(400).json('Correo ya existe');
-
-        try {
-            const savedUser = await this.assingRol(req, res);
-            await savedUser.save();
-
-            const token: string = jwt.sign({ _id: savedUser._id }, envs.TOKEN_SECRET || 'tokentest', {
-                expiresIn: 60 * 60 * 24});
-
-            res.header('auth-token', token).json(savedUser); 
-        } catch (error) {
-            res.status(500).json(error);
+export class AuthController{
+    
+    async signUp(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined> {
+        
+        const emailExistsAuthWrapper = await AuthenticableWrapper.findOne({ email: req.body.email });   
+        const emailExistUser = await User.findOne({ email: req.body.email });
+        if (emailExistsAuthWrapper || emailExistUser) {
+            return res.status(400).json('Correo ya existe');
+        }
+        else{
+            try {
+                const auth: IAuthenticable= new AuthenticableWrapper(req.body);
+                return auth.signUp(req, res);
+            } catch (error) {
+                res.status(400).json({ error: error });
+            }
+            
+            
+        }
+    }
+    async signIn(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined> {
+        const emailExistsAuthWrapper = await AuthenticableWrapper.findOne({ email: req.body.email });
+        const emailExistUser = await User.findOne ({ email: req.body.email });
+        if (emailExistsAuthWrapper) {
+            return emailExistsAuthWrapper.signIn(req, res);
+        }
+        else if(emailExistUser){
+            return emailExistUser.signIn(req, res);
+        }
+        else{
+            return res.status(400).json('Correo no existe');
+        }
+    }
+    async profile(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined> {  
+        const emailExistsAuthWrapper = await AuthenticableWrapper.findOne({ email: req.body.email });
+        const emailExistUser = await User.findOne ({ email: req.body.email });
+        if (emailExistsAuthWrapper) {
+            return emailExistsAuthWrapper.profile(req, res);
+        }
+        else if(emailExistUser){
+            return emailExistUser.profile(req, res);
+        }
+        else{
+            return res.status(400).json('Correo no existe');
         }
     }
 
-    public signIn = async (req: Request, res: Response) => {
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) return res.status(400).json('El email no es válido.');
 
-        const correctPassword: boolean = await user.validatePassword(req.body.password);
-        if (!correctPassword) return res.status(400).json('Contraseña invalida');
-
-        const token: string = jwt.sign({ _id: user._id }, envs.TOKEN_SECRET || 'tokentest', {
-            expiresIn: 60 * 60 * 24
-        });
-
-        res.set('Access-Control-Expose-Headers', 'auth-token');
-        res.header('auth-token', [token]).json(user);
-        
-        console.log(req.body);
-    }
-
-    public profile = async (req: Request, res: Response) => {
-        const user = await User.findById(req.userId);
-        if(!user) 
-            return res.status(404).json('Usuario no encontrado');
-
-        res.json(user);
-    }
-
-    public assingRol = async (req: Request, res: Response) => {
-        let newUser: IUser;
-        (req.body.rol === rol.PROFESOR_GUIA) 
-        ? newUser = new ProfesorUser(req.body) 
-        : newUser = new AdminUser(req.body);
-
-        newUser.password = await newUser.encryptPassword(newUser.password);
-        return newUser;
-    } 
 }
