ae0986c52994054774bbbde0ed12d2f96dd7dd72
Rafael Vargas <vbr.vargas31@gmail.com>
2024-06-15 21:57:32 -0600

    implementacion visitorPublication

--- src/services/visitor.service.ts
+++ src/services/visitor.service.ts
@@ -1,5 +1,6 @@
 import { MessageVisitor } from "../models/automation/visitor.model";
 import { IActivity } from "../models/activity.model";
+import { ActivityStatus } from "../models/activity.model";
 
 export class AcceptVisitorReminder implements MessageVisitor {
     visit(activity: IActivity): void {
@@ -9,6 +10,15 @@ export class AcceptVisitorReminder implements MessageVisitor {
 
 export class AcceptVisitorPublication implements MessageVisitor {
     visit(activity: IActivity): void {
-        return;
+        const currentDate = new Date();
+        
+        const diffDays = Math.floor((activity.date.getTime() - currentDate.getTime()) / (1000 * 3600 * 24));
+        
+        if (diffDays <= activity.daysToAnnounce) {
+            
+            activity.status = ActivityStatus.NOTIFICADA;
+
+            //CentroNotificaciones.notify(activity);
+        }
     }
 }
